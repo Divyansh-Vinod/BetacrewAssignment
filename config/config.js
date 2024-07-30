bb5e539debb13c96d95e1fc0aa37755cdd909400
Divyansh Vinod <divyanshjns@gmail.com>
2024-07-30 22:57:10 +0530

    config

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SERVER_HOST: "localhost",
+  SERVER_PORT: 3000,
+  CALL_TYPE_STREAM_ALL: 1,
+  CALL_TYPE_RESEND: 2,
+  PACKET_SIZE: 17, // Packet size based on server specification
+};
